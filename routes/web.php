24a15572557664dade14f11626a5dce226a12e61
alejandrohd <alejandrohernandezdeveloper@gmail.com>
2025-05-23 17:19:10 -0500

    Se corrige ruta principal

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Mail;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
